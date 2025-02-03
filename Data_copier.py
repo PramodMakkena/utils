4ab9f3bdf106dcbe2691e57cbbae70ab4283e7b1
gauravpant1306 <45787597+gauravpant1306@users.noreply.github.com>
2025-02-04 00:30:12 +0530

    Update Data_copier.py

--- Data_copier.py
+++ Data_copier.py
@@ -371,25 +371,25 @@ def create_tbl(
                 log_msg(
                     f"ERROR: Source table {src_db or 'None'}.{src_ds or 'None'}.{src_tbl or 'None'}  or {file_name or 'None'} does not exist.")
                 return
-        
-            # 1 (if key_cols is not present when key_vals has data)
-            if key_vals:
-                if not key_cols:
-                    log_msg(
-                        f"ERROR: key_cols {key_cols} does not exist.")
-                    return
-        
+
+            # # 1 (if key_cols is not present when key_vals has data)
+            # if key_vals:
+            #     if not key_cols:
+            #         log_msg(
+            #             f"ERROR: key_cols {key_cols} does not exist.")
+            #         return
+
             # 1 (to check both)
-            # if not key_cols:  # when key_cols do not exist (key_vals may or may not be present)
-            #     log_msg(
-            #         f"ERROR: key_cols {key_cols} does not exist.")
-            #     return
-            #
-            # if not key_vals:  # when key_cols exist but key_vals does not
-            #     log_msg(
-            #         f"ERROR: key_vals {key_vals} does not exist.")
-            #     return
-        
+            if not key_cols:  # when key_cols do not exist (key_vals may or may not be present)
+                log_msg(
+                    f"ERROR: key_cols {key_cols} does not exist.")
+                return
+
+            if not key_vals:  # when key_cols exist but key_vals does not
+                log_msg(
+                    f"ERROR: key_vals {key_vals} does not exist.")
+                return
+
             default_limit = 1000
             max_limit = 10000
             if not col_list:  # 2
@@ -402,20 +402,20 @@ def create_tbl(
                             limit_val = min(limit_val, max_limit)
                     else:
                         limit_val = int(default_limit)
-        
+
             where_str = getWhereCond(tgt_ds, tgt_tbl, src_db, src_ds, src_tbl, col_list, key_cols, key_vals)
             limit_str = getLimit(limit_val)
             sqlstr = ("select " + col_list + " from "
                       + src_db + "." + src_ds + "." + src_tbl
                       + where_str + limit_str)
             bq_to_csv(bq_client, project_id, csv_path, file_name, sqlstr)
-    
+
             # Load CSV to BQ
         elif crt_rep.upper() == "CSV_TO_BQ":
             file_path = f"gs://{project_id}/{csv_path}{file_name}"
             df = readfrombucket(spark, file_path, "csv")
             csv_to_bq(df, schema_json, project_id, tgt_ds, tgt_tbl)
-    
+
     except Exception as e:
         err_str = "Exception in create_tbl: " + str(e)
         throw_exception(err_str)
