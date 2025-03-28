75d9846e59ad45a9c6599e73647c565126e62f22
GCP_Python <pramodaoran@gmail.com>
2025-03-28 11:22:35 -0700

    Update master_table_creation.py

--- master_table_creation.py
+++ master_table_creation.py
@@ -2,172 +2,125 @@ import sys
 import json
 from gfcc_dam_utility import *
 import gfcc_dam_utility
-from google.cloud import storage
+from google.cloud import storage, bigquery
 from pyspark.sql.types import StringType, IntegerType, DecimalType, BooleanType, TimestampType, LongType, DateType
 from pyspark.sql.functions import regexp_replace, col, to_timestamp
 from datetime import datetime
 import pandas as pd
-import ast
+import os
 
 global dttm
 
-
 def getArgs():
-    log_msg("------------------ Starting getArgs function to get arguments ------------------")
     try:
         # Fetching all the arguments
         args_dict = eval(sys.argv[1])
         project_id = args_dict["project_id"]
         bucket = args_dict["bucket"]
-        f_name = args_dict["f_name"]
-        print("args ", sys.argv)
+        master_table_file_name = args_dict["master_table_file_name"]
+        dag_table_ids = args_dict["table_ids"]
 
-        # config_subpath is just an example of subpath usage
+        print("args", sys.argv)
+        
         config_subpath = f"gs://{bucket}/dags/lumiteam-usecase/gfcc_dam_config_files/"
-        dc_config_file = 'gfcc_dam_dc_config.json'
-
-        # Reading Global Config
+        dc_config_file = "gfcc_dam_dc_master_table_config.json"
         gfcc_dam_utility.global_config_file_name = args_dict["global_config_file_name"]
+        
         lumi_id = getLumiSpace(project_id)
+        log_msg("project_id: " + str(project_id))
+        log_msg("bucket: " + str(bucket))
+        log_msg("lumi_id: " + str(lumi_id))
 
-        # Printing the received arguments
-        log_msg('project_id: ' + str(project_id))
-        log_msg('bucket: ' + str(bucket))
-        log_msg('lumi_id: ' + str(lumi_id))
-        log_msg("------------------ Getting arguments completed ------------------")
-        return project_id, bucket, config_subpath, dc_config_file, lumi_id, f_name
-
+        return project_id, bucket, config_subpath, dc_config_file, lumi_id, master_table_file_name, dag_table_ids
     except Exception as e:
         err_str = "Exception in getArgs: " + str(e)
         throw_exception(err_str)
 
-
 def readconfig():
-    log_msg("------------------ Starting reading configurations to get variables from config  ------------------")
     try:
-        with open(dc_config_file, 'r') as f:
+        with open(dc_config_file, "r") as f:
             config = json.load(f)
-        dc_ids = config['dc_ids']
-        csv_location = config['csv_loc']
-        return dc_ids, csv_location
-
+            table_ids = config["table_ids"]
+            csv_location = config["csv_loc"]
+            return table_ids, csv_location
     except Exception as e:
         err_str = "Exception in readconfig: " + str(e)
         throw_exception(err_str)
 
-
 def readCSV(spark):
-    """
-    Reads the CSV with data_copier instructions, iterates over each dc_id,
-    and processes each entry. If src_db, src_ds, src_tbl is missing or does not exist,
-    it will log an error and continue with the next entry.
-    """
     try:
-        # Determine CSV location
-        if len(f_name) > 0:
-            dc_csv_location_path = f"gs://{project_id}{csv_location}{f_name}"
-        else:
-            dc_csv_location_path = f"gs://{project_id}{csv_location}data_copier.csv"
-
+        dc_csv_location_path = f"gs://{project_id}/{csv_location}master_table.csv"
         log_msg(dc_csv_location_path)
+        df = readFromBucket(spark, dc_csv_location_path, "csv")
+        dtm = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")
+        log_msg(dttm)
 
-        # Read CSV into DataFrame
-        df = readfrombucket(spark, dc_csv_location_path, "csv")
-        if df.count() == 0:
-            log_msg("All rows are invalid. DAG stopped.")
-            return
+        valid_run = 1
+        if len(dag_table_ids) > 0:
+            table_ids = dag_table_ids.split(",")
+            print("table_ids:", table_ids)
+        else:
+            log_msg("ERROR: table_id not provided in DAG parameter, kindly provide and re-run")
+            valid_run = 0
 
-        # Generate a UTC timestamp for suffix naming
-        dttm = datetime.utcnow().strftime('%Y%m%d_%H%M%S')
-        print("Current timestamp for backups: ", dttm)
-
-        data = df.collect()
-
-        for row in data:
-            tbl_name = str(row.tbl_name)
-            pk = str(row.pk)
-            clmn_names = str(row.clmn_names)
-
-            try:
-                create_tbl(
-                    tbl_name,
-                    pk,
-                    clmn_names
-                )
-            except Exception as e:
-                # This ensures that if something fails inside create_tbl,
-                # we only skip this iteration and continue with next row.
-                log_msg(f"ERROR while processing table: {tbl_name}. Skipping this record. Error details: {e}")
-                continue
-
+        if valid_run == 1:
+            for val in table_ids:
+                if val == "all":
+                    data = df.collect()
+                else:
+                    data = df.filter(df.table_id == val).collect()
+                
+                for row in data:
+                    table_id = row.table_id
+                    table_name = str(row.table_name)
+                    pks = row.pks if pd.notna(row.pks) else ""
+                    log_msg("table_id: " + str(table_id))
+                    log_msg("table_name: " + str(table_name))
+                    log_msg("pks: " + str(pks))
+                    create_master_table(spark, table_name, pks)
     except Exception as e:
         err_str = "Exception in readCSV: " + str(e)
         throw_exception(err_str)
 
-
-
-def create_tbl(
-        tbl_name,
-        pk,
-        clmn_names
-):
+def create_master_table(spark, table_name, pks):
     try:
-        log_msg("------------------ Creating tables------------------")
-        final_schema = {}
-        primary_key_constraint = f", PRIMARY KEY({pk})" if pk else ""
-        for col_info in clmn_names.split(","):
-            col_info = col_info.strip()
-            col, col_datatype = col_info.split(":")
-            col = col.strip()
-            col_datatype = col_datatype.strip()
-            final_schema[col] = col_datatype
+        if all(var != "None" for var in [table_name]):
+            ddl_master_tbl = f"""
+            CREATE TABLE IF NOT EXISTS `{project_id}.data.master_table` (
+                table_name STRING NOT NULL,
+                primary_keys STRING,
+                PRIMARY KEY(table_name) NOT ENFORCED
+            )
+            """
+            execute_bq(bq_client, ddl_master_tbl)
 
-        col_definitions = "{" + ",".join(f"\"{col}\":\"{dtype}\""
-                                         for col, dtype in final_schema.items()) + "}"
-
-        ddl_str = "INSERT INTO `data.master_table` (table_name, schema_definition, primary_keys) VALUES({},{},{})".format(
-            tbl_name, col_definitions, primary_key_constraint)
-        print(ddl_str)
-        bq_client.query(ddl_str)
-
-        print(ddl_str)
-        bq_client.query(ddl_str)
-
+            merge_ddl = f"""
+            MERGE `{project_id}.data.master_table` AS target
+            USING (SELECT '{table_name}' AS table_name, '{pks}' AS primary_keys) AS source
+            ON target.table_name = source.table_name
+            WHEN MATCHED THEN UPDATE SET target.primary_keys = source.primary_keys
+            WHEN NOT MATCHED THEN INSERT (table_name, primary_keys) VALUES (source.table_name, source.primary_keys)
+            """
+            execute_bq(bq_client, merge_ddl)
+        else:
+            log_msg("ERROR: Values for table_name is/are not given. Please update the master_table.csv with appropriate values for this table_id")
     except Exception as e:
-        err_str = "Exception in create_tbl: " + str(e)
+        err_str = "Exception in insert_master_tbl: " + str(e)
         throw_exception(err_str)
 
-
 def main():
-    log_msg("------------------ Starting main function ------------------")
-    global spark, project_id, bucket, config_subpath, dc_config_file, csv_location
-    global f_name, project_ds, lumi_id, bq_client, lumi_ds, dc_ids
-    # These might be used if declared globally, or you can store them in a local scope
+    log_msg("Starting main function")
+    global spark, project_id, bucket, config_subpath, dc_config_file, csv_location, master_table_file_name, dag_table_ids, table_ids, table_id, csv_path, dtm
 
-    # 1. Get parameters
-    project_id, bucket, config_subpath, dc_config_file, lumi_id, f_name = getArgs()
-
-    # 2. Get project DS & LUMI DS from config
+    project_id, bucket, config_subpath, dc_config_file, lumi_id, master_table_file_name, dag_table_ids = getArgs()
     project_ds = getproject_ds(project_id)
     lumi_ds = getlumi_ds(project_id)
 
-    # 3. Initialize Spark & BQ client
-    spark = get_conn_spark()
-    bq_client = get_conn_bq()
-
-    # 4. Set up a temporary GCS bucket for BQ to Spark
-    temp_bucket = project_id + "-temp"
-    tempBucket(spark, bucket=temp_bucket)
-
-    # 5. Read the config JSON (dc_config_file) to get CSV location and dc_ids
-    dc_ids, csv_location = readconfig()
-    print(dc_ids)
-    print(csv_location)
-    print(f_name)
-
-    # 6. Read the data_copier CSV and process instructions
+    # Creating spark and bigquery client connection
+    spark = getSparkSession()
+    bq_client = getBQClient()
+    table_ids, csv_location = readconfig()
     readCSV(spark)
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
